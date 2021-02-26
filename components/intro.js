54de99b6bc1c0bf7c0492b700b7f03401b213da0
Letlhogonolo123 <you@example.com>
2021-02-26 08:33:11 +0200

    First Commit Message

---
+++ components/intro.js
@@ -0,0 +1,10 @@
+export default function Intro() {
+  return (
+    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+      <h2 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+        Blog Supported by DatoCMS and Ft
+      </h2>
+
+    </section>
+  )
+}
